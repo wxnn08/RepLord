d32274a3b8fed8f22f7dad01a9d0905a66ddf1c3
Wesley <wesleypersilva@gmail.com>
2018-04-09 20:55:22 -0300

    add PI e INF

--- base.cpp
+++ base.cpp
@@ -7,7 +7,9 @@ using namespace std;
 #define sec second
 
 typedef long long ll;
- 
+const int INF = 0x3f3f3f3f;
+const double PI = acos(-1.0);
+
 int main(){
     ios_base::sync_with_stdio(false);
 
