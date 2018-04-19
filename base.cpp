e3be116695748a88c553acaccdcd6fc653461dbb
Wesley <wesleypersilva@gmail.com>
2018-04-19 11:17:51 -0300

    add define for_

--- base.cpp
+++ base.cpp
@@ -5,11 +5,12 @@ using namespace std;
 #define mk make_pair
 #define fi first
 #define sec second
+#define for_(i, a, b) for(int i = int(a); i < int(b); i++)
 
 typedef long long ll;
 const int INF = 0x3f3f3f3f;
 const double PI = acos(-1.0);
-
+ 
 int main(){
     ios_base::sync_with_stdio(false);
 
