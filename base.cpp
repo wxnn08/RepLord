666c924cfd377a4eab0a5e193ca650153ab5d1d3
Wesley <wesleypersilva@gmail.com>
2019-02-04 23:17:47 -0200

    att

--- base.cpp
+++ base.cpp
@@ -5,17 +5,16 @@ using namespace std;
 #define eb emplace_back
 #define mk make_pair
 #define fi first
-#define sec second
-#define fori(i, a, b) for(int i = int(a); i < int(b); i++)
+#define se second
 #define cc(x)	cout << #x << " = " << x << endl
 #define ok		cout << "ok" << endl
 
-typedef pair<int,int> ii;
 typedef long long ll;
+typedef pair<int,int> ii;
 const int INF = 0x3f3f3f3f;
 const double PI = acos(-1.0);
  
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
 
     
